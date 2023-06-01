96c59355a351f15a3216a98ccd477be881652971
SovicLeon <sovicleon@gmail.com>
2023-06-01 21:48:59 +0200

    Road info now has toggle to only show data of logged in user

--- src/components/RoadInfo.js
+++ src/components/RoadInfo.js
@@ -4,7 +4,7 @@ import { Polyline } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS } from 'chart.js/auto'
+import { Chart as ChartJS } from 'chart.js/auto';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { UserContext } from '../userContext';
@@ -19,48 +19,55 @@ L.Icon.Default.mergeOptions({
 
 async function fetchRoadInfo() {
   try {
-    const res = await fetch("http://localhost:3001/sections", {
-      method: "GET",
-      credentials: "include",
+    const res = await fetch('http://localhost:3001/sections', {
+      method: 'GET',
+      credentials: 'include',
       headers: { 'Content-Type': 'application/json' },
     });
 
     const data = await res.json();
-    console.log("Response data:", data);
+    console.log('Response data:', data);
 
     if (Array.isArray(data) && data.length > 0) {
       return data;
     } else {
-      throw new Error("Invalid response data");
+      throw new Error('Invalid response data');
     }
   } catch (error) {
-    console.error("Error fetching last road info:", error);
+    console.error('Error fetching last road info:', error);
     throw error;
   }
 }
 
 function RoadInfo() {
-  const userContext = useContext(UserContext); 
+  const userContext = useContext(UserContext);
   const [fromDate, setFromDate] = useState(null);
   const [toDate, setToDate] = useState(null);
   const [userPositions, setUserPositions] = useState([]);
   const [loading, setLoading] = useState(true);
-  
+  const [showFilteredData, setShowFilteredData] = useState(false); // New state
+
   useEffect(() => {
     fetchRoadInfo()
-      .then(positions => {
+      .then((positions) => {
         setUserPositions(positions);
         setLoading(false);
       })
-      .catch(error => {
+      .catch((error) => {
         setLoading(false);
       });
   }, []);
 
   if (loading) {
-    return <div className="progress-bar" style={{ '--width': 10 }} data-label="Loading..."></div>;
+    return (
+      <div
+        className="progress-bar"
+        style={{ '--width': 10 }}
+        data-label="Loading..."
+      ></div>
+    );
   }
-  
+
   var filteredPositions = userPositions.filter((position) => {
     const positionDate = new Date(position.time);
     return (
@@ -69,13 +76,13 @@ function RoadInfo() {
     );
   });
 
-  if (userContext.user) {
+  if (showFilteredData && userContext.user) { // Show filtered data if the user is logged in and toggle is enabled
     filteredPositions = userPositions.filter((position) => {
       const positionDate = new Date(position.time);
       return (
         (!fromDate || positionDate >= fromDate) &&
         (!toDate || positionDate <= toDate) &&
-        (position.postedBy._id === userContext.user._id)
+        position.postedBy._id === userContext.user._id
       );
     });
   }
@@ -88,6 +95,10 @@ function RoadInfo() {
   const handleToDateChange = (event) => {
     const selectedDate = new Date(event.target.value);
     setToDate(selectedDate);
+  };
+
+  const handleToggleChange = () => {
+    setShowFilteredData((prevState) => !prevState); // Toggle the state
   };
 
   const chartData = {
@@ -108,16 +119,16 @@ function RoadInfo() {
 
   return (
     <div>
-      <div class="pageTitle">
+      <div className="pageTitle">
         <h1>Road info</h1>
       </div>
-      <div class="datePicker">
+      <div className="datePicker">
         <label htmlFor="fromDate">From Date:</label>
         <input
           type="date"
           id="fromDate"
           name="fromDate"
-          class="dateInput"
+          className="dateInput"
           onChange={handleFromDateChange}
         />
         <label htmlFor="toDate">To Date:</label>
@@ -125,17 +136,36 @@ function RoadInfo() {
           type="date"
           id="toDate"
           name="toDate"
-          class="dateInput"
+          className="dateInput"
           onChange={handleToDateChange}
         />
+        {userContext.user && ( // Render the toggle button only if the user is logged in
+        <div className="toggleContainer">
+          <label>
+            Show Only My Data:
+            <input
+              type="checkbox"
+              checked={showFilteredData}
+              onChange={handleToggleChange}
+            />
+          </label>
+        </div>
+      )}
       </div>
       <div className="mapContainer">
-        <MapContainer center={[userPositions[0].start_pos_lat, userPositions[0].start_pos_lon]} zoom={13} style={{ height: "500px" }}>
+        <MapContainer
+          center={[
+            userPositions[0].start_pos_lat,
+            userPositions[0].start_pos_lon,
+          ]}
+          zoom={13}
+          style={{ height: '500px' }}
+        >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-           {filteredPositions.map((position, idx) => (
+          {filteredPositions.map((position, idx) => (
             <>
               <Polyline
                 key={`polyline-${idx}`}
@@ -156,30 +186,41 @@ function RoadInfo() {
                 }}
                 positions={[
                   [position.start_pos_lat, position.start_pos_lon],
-                  [position.end_pos_lat, position.end_pos_lon]
+                  [position.end_pos_lat, position.end_pos_lon],
                 ]}
               >
                 <Popup>
                   Start Position: <br />
-                  Lat: {position.start_pos_lat}, Long: {position.start_pos_lon} <br />
+                  Lat: {position.start_pos_lat}, Long: {position.start_pos_lon}{' '}
+                  <br />
                   Time: {new Date(position.time).toLocaleString()} <br />
                   Duration: {position.duration} <br />
-                  Acceleration Average: {position.acc_average}, Max: {position.acc_max}, Min: {position.acc_min} <br />
+                  Acceleration Average: {position.acc_average}, Max:{' '}
+                  {position.acc_max}, Min: {position.acc_min} <br />
                 </Popup>
               </Polyline>
-              <Marker key={`marker-start-${idx}`} position={[position.start_pos_lat, position.start_pos_lon]}>
+              <Marker
+                key={`marker-start-${idx}`}
+                position={[position.start_pos_lat, position.start_pos_lon]}
+              >
                 <Popup>
                   Start Position: <br />
-                  Lat: {position.start_pos_lat}, Long: {position.start_pos_lon} <br />
+                  Lat: {position.start_pos_lat}, Long: {position.start_pos_lon}{' '}
+                  <br />
                   Time: {new Date(position.time).toLocaleString()} <br />
                   Duration: {position.duration} <br />
-                  Acceleration Average: {position.acc_average}, Max: {position.acc_max}, Min: {position.acc_min} <br />
+                  Acceleration Average: {position.acc_average}, Max:{' '}
+                  {position.acc_max}, Min: {position.acc_min} <br />
                 </Popup>
               </Marker>
-              <Marker key={`marker-end-${idx}`} position={[position.end_pos_lat, position.end_pos_lon]}>
+              <Marker
+                key={`marker-end-${idx}`}
+                position={[position.end_pos_lat, position.end_pos_lon]}
+              >
                 <Popup>
                   End Position: <br />
-                  Lat: {position.end_pos_lat}, Long: {position.end_pos_lon} <br />
+                  Lat: {position.end_pos_lat}, Long: {position.end_pos_lon}{' '}
+                  <br />
                   Time: {new Date(position.timestamp).toLocaleString()} <br />
                 </Popup>
               </Marker>
